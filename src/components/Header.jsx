597bc5f90c5bf2d6adb04b70166368bb470f167f
ZaraisGOLD <zaraisgold@gmail.com>
2023-05-09 19:58:50 -0400

    darkmode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/header.css'
 
-const handleMenu = () => {
-	const menu = document.querySelector(".navbar__menu");
-    const iconMenu = document.querySelector(".bx-menu");
-
-    iconMenu.addEventListener('click', function () {
-        menu.classList.toggle('navbar__menu--show');
-    });
-}
-
 const Header = () => {
-	return (
-		<header>
-			<nav className="navbar">
-				<a className="navbar__logo" href="home">
-					<img src="/sol.svg" alt="logo" />
-				</a>
-				<ul className="navbar__menu" >
-					<li className='navbar__menu--list'><a href="home">Home</a></li>
-					<li className='navbar__menu--list'><a href="projects">Projects</a></li>
-					<li className='navbar__menu--list'><a href="about">About</a></li>
-					<li className='navbar__menu--list'><a href="contact">Contact</a></li>
-				</ul>
-				<i onClick={handleMenu} className='bx bx-menu'></i>
-				{/* <img className='menu__icon--luna' src="/luna.svg" alt="" />
-				<img className='menu__icon--sol' src="/sol.svg" alt="" /> */}
-			</nav>
-		</header>
-	)
+
+  const [menu, setMenu] = useState(false)
+  const [darkMode, setDarkMode] = useState(false)
+
+  const handleMenu = () => {
+    setMenu(!menu)
+  }
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
+  return (
+    <header>
+      <nav className="navbar">
+        <a className="navbar__logo" href="home">
+          <img src="/logo.svg" alt="logo" />
+        </a>
+        <ul className={`navbar__menu ${menu ? 'open' : ''}`} >
+          <li className='navbar__menu--list'><a href="home">Home</a></li>
+          <li className='navbar__menu--list'><a href="projects">Projects</a></li>
+          <li className='navbar__menu--list'><a href="about">About</a></li>
+          <li className='navbar__menu--list'><a href="contact">Contact</a></li>
+        </ul>
+        <div className='navbar__icons'>
+          <div className='icons__moonSun'>
+            <img onClick={handleDarkMode} className={`menu__icon--luna ${darkMode ? 'night' : ''}`} src="/luna.svg" alt="" />
+            <img onClick={handleDarkMode} className={`menu__icon--sol ${darkMode ? 'day' : ''}`} src="/sol.svg" alt="" />
+          </div>
+          <i onClick={handleMenu} className='bx bx-menu'></i>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,28 +2,42 @@ import React from 'react'
 import './styles/main.css'
 
 const Main = () => {
-    return (
-        <>
-            <section className='main__home' id="Home">
-                <div></div>
-                <h1>Hi!, I'm Zarais</h1>
-                <h2>Home</h2>
-                <p></p>
-            </section>
-            <section className='main__portfolio' id="Portfolio">
-                <h2>Projects</h2>
-                <div></div>
-            </section>
-            <section className='main__about' id="About">
-                <div></div>
-                <h2>About</h2>
-            </section>
-            <section className='main__contact' id="Contact">
-                <h2>Contact</h2>
-                <div></div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className='main__home' id="Home">
+        <div className='home__container'>
+          <div className='home'>
+            <h1 className='home__title'>Hi!, I'm Zarais</h1>
+            <p className='home__paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem deleniti, quae reiciendis quasi quia necessitatibus blanditiis sit perferendis aut eveniet libero, quidem voluptas. Soluta natus voluptate illo velit omnis.</p>
+          </div>
+        </div>
+      </section>
+      <section className='main__projects' id="Projects">
+        <div className='projects__container'>
+          <div className='projects'>
+            <h1 className='projects__title'>Projects</h1>
+            <p className='projects__paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem deleniti, quae reiciendis quasi quia necessitatibus blanditiis sit perferendis aut eveniet libero, quidem voluptas. Soluta natus voluptate illo velit omnis.</p>
+          </div>
+        </div>
+      </section>
+      <section className='main__about' id="About">
+        <div className='about__container'>
+          <div className='about'>
+            <h1 className='about__title'>About</h1>
+            <p className='about__paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem deleniti, quae reiciendis quasi quia necessitatibus blanditiis sit perferendis aut eveniet libero, quidem voluptas. Soluta natus voluptate illo velit omnis.</p>
+          </div>
+        </div>
+      </section>
+      <section className='main__contact' id="Contact">
+        <div className='contact__container'>
+          <div className="contact">
+            <h1 className='contact__title'>Contact</h1>
+            <p className='contact__paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem deleniti, quae reiciendis quasi quia necessitatibus blanditiis sit perferendis aut eveniet libero, quidem voluptas. Soluta natus voluptate illo velit omnis.</p>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Main
